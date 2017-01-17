e5f8b33327a7c8c5904b0b5690e890ce85efba0f
Siddhartha Basu <biosidd@gmail.com>
2017-01-17 19:58:53 +0000

    added check for empty slice

--- aphcollection/collection.go
+++ aphcollection/collection.go
@@ -5,6 +5,9 @@ package aphcollection
 // Index returns the index of the first instance of s in slice a, or -1 if s is
 // not present in a
 func Index(a []string, s string) int {
+	if len(a) == 0 {
+		return -1
+	}
 	for i, v := range a {
 		if v == s {
 			return i
@@ -15,12 +18,18 @@ func Index(a []string, s string) int {
 
 // Contains reports whether s is present in slice a
 func Contains(a []string, s string) bool {
+	if len(a) == 0 {
+		return false
+	}
 	return Index(a, s) >= 0
 }
 
 // Map applies the given function to each element of a, returning slice of
 // results
 func Map(a []string, fn func(string) string) []string {
+	if len(a) == 0 {
+		return a
+	}
 	sl := make([]string, len(a))
 	for i, v := range a {
 		sl[i] = fn(v)
